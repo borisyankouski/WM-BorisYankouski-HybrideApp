82369007088df00be77df690241de2728d228f1f
Boris-Yankouski <b.yankouski@gmail.com>
2023-11-13 18:06:33 +0100

    Delete alert added
    next TO-DO toast

--- hybride-app/src/components/ProjectCard.vue
+++ hybride-app/src/components/ProjectCard.vue
@@ -11,16 +11,18 @@
             <ion-button fill="clear" color="tertiary" aria-label="edit" @click="openProjectModal">
                 Beheer <ion-icon slot="end" :icon="cogOutline" color="tertiary"></ion-icon>
             </ion-button>
-            <ion-button fill="clear" color="danger" aria-label="delete" @click="deleteProject(pr_id)">
+            <ion-button :id="'deleteProjectBtn_' + pr_id" fill="clear" color="danger" aria-label="delete" @click="handleDeleteClick">
                 Verwijder <ion-icon slot="end" :icon="trashOutline" color="danger"></ion-icon>
             </ion-button>
         </ion-row>
+        <ion-alert :trigger="'deleteProjectBtn_' + pr_id" v-model="deleteAlertVisible" header="Project Verwijderen"
+            message="Deze actie kan niet ongedaan worden!" :buttons="alertButtons"></ion-alert>
     </ion-card>
 </template>
     
 <script setup>
 import { defineProps, defineEmits, inject, ref } from 'vue';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonRow } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonRow, IonAlert } from '@ionic/vue';
 import { cogOutline, trashOutline } from 'ionicons/icons';
 
 const { pr_naam, pr_code, pr_omschrijving, pr_id } = defineProps(['pr_naam', 'pr_code', 'pr_omschrijving', 'pr_id']);
@@ -35,24 +37,70 @@ const openProjectModal = () => {
     });
 };
 
+const deleteAlertVisible = ref(false);
+
+const showDeleteAlert = () => {
+    console.log('Showing delete alert');
+    deleteAlertVisible.value = true;
+};
+
+const hideDeleteAlert = () => {
+    deleteAlertVisible.value = false;
+};
+
+const handleDeleteClick = () => {
+    showDeleteAlert();
+};
+
+const alertButtons = [
+    {
+        text: 'Annuleren',
+        role: 'cancel',
+        color: 'tertiary',
+        handler: () => {
+            hideDeleteAlert();
+        },
+    },
+    {
+        text: 'Verwijderen',
+        cssClass: 'alertBtnConfirm',
+        handler: () => {
+            deleteProject(pr_id);
+            hideDeleteAlert();
+        },
+    },
+];
+
 const axios = inject('axios');
 const deleteProject = (projectID) => {
-  axios
-    .delete('https://www.kovskib.com/MW/RESTfulAPI/api/PROJECTEN.php',  {
-        data: { pr_id: projectID }
-    })
-    .then(response => {
-      if (response.status !== 200) {
-        console.log('Error tijdens het verwijderen van project' + response.status)
-      }
-      console.log('Project removed');
-      emit('projectenUpdated');
-    });
+    axios
+        .delete('https://www.kovskib.com/MW/RESTfulAPI/api/PROJECTEN.php', {
+            data: { pr_id: projectID }
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('Error tijdens het verwijderen van project' + response.status)
+            }
+            console.log('Project removed');
+            emit('projectenUpdated');
+        });
 }
 </script>
   
-<style scoped>
+<style>
 #btnDelete button {
     align-self: flex-end;
+}
+
+.alert-wrapper {
+    border: #f4f5f8 1px solid;
+}
+
+.alert-button-role-cancel {
+    color: #3dc2ff !important;
+}
+
+.alertBtnConfirm {
+    color: #eb445a !important;
 }
 </style>
